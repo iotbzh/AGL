3f3c570e7e51a8d019a791d047ee79fcb48cacfb
Manuel Bachmann <mbc@iot.bzh>
2015-09-17 10:55:53 +0000

    Add a hint for the download URL in the Porter build scripts
    Signed-off-by: Manuel Bachmann <mbc@iot.bzh>

--- build_porter.sh
+++ build_porter.sh
@@ -5,7 +5,7 @@ git submodule update
 
 #porter section
 echo -e "\n"
-echo -e "Please copy these 2 files in your \"$HOME\" directory:"
+echo -e "Please download these 2 files from \"http://www.renesas.com/secret/r_car_download/rcar_demoboard.jsp\" and copy them into your \"$HOME\" directory:"
 echo -e "* R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip"
 echo -e "* R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip"
 echo -e "and press [ENTER] to continue...\n"
