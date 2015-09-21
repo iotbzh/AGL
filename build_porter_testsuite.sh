5c922e8e470af3725be36a6b1833e0fe73797af4
Manuel Bachmann <manuel.bachmann@iot.bzh>
2015-09-21 10:03:51 +0000

    Initial "testsuite" branch, with script for testsuite image
    Do :
    $ git checkout testuiste
    $ ./build_porter_testsuite.sh
    
    Enjoy :).
    
    Signed-off-by: Manuel Bachmann <manuel.bachmann@iot.bzh>

---
+++ build_porter_testsuite.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+
+#porter section
+echo -e "\n"
+echo -e "Please download these 2 files from \"http://www.renesas.com/secret/r_car_download/rcar_demoboard.jsp\" and copy them into your \"$HOME\" directory:"
+echo -e "* R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip"
+echo -e "* R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip"
+echo -e "and press [ENTER] to continue...\n"
+read
+mkdir binary-tmp
+cd binary-tmp
+unzip -o "$HOME/R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip"
+unzip -o "$HOME/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip"
+cd ../meta-renesas/meta-rcar-gen2
+./copy_gfx_software_porter.sh ../../binary-tmp
+./copy_mm_software_lcb.sh ../../binary-tmp
+cd ../..
+
+export TEMPLATECONF=$PWD/meta-renesas/meta-rcar-gen2/conf
+source poky/oe-init-build-env
+
+# overload OE variable DL_DIR from environment
+[ -d "$DL_DIR" ] &&
+	export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE DL_DIR"
+
+# overload OE variable SSTATE_DIR from environment
+[ -d "$SSTATE_DIR" ] &&
+	export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE SSTATE_DIR"
+
+bitbake agl-demo-platform-testsuite
