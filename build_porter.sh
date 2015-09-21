6eb452047c065eb17a70de58c3c4137bb445bbdd
Yannick GICQUEL <yannick.gicquel@iot.bzh>
2015-09-21 15:55:37 +0200

    register upstream git repositories in related layers
    applied to:
    - meta-agl
    - meta-agl-demo
    - meta-renesas
    
    Signed-off-by: Yannick GICQUEL <yannick.gicquel@iot.bzh>

--- build_porter.sh
+++ build_porter.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+# syntax:
+#  add_upstream_remote <sub-directory> <git_repo_url>
+function add_upstream_remote
+{
+	directory=$1
+	repo=$2
+
+	[ -d $directory ] && {
+		cd $directory
+		git remote -v | grep upstream > /dev/null ||
+			git remote add upstream $repo
+		cd - > /dev/null
+	}
+}
+
+# Script entry point
 git submodule init
 git submodule update
+
+# Register "upstream" remote for AGL layers
+add_upstream_remote meta-agl https://gerrit.automotivelinux.org/gerrit/AGL/meta-agl
+add_upstream_remote meta-agl-demo https://gerrit.automotivelinux.org/gerrit/AGL/meta-agl-demo
+add_upstream_remote meta-renesas https://gerrit.automotivelinux.org/gerrit/AGL/meta-renesas
 
 #porter section
 echo -e "\n"
