d8b48231169c75c7b4e99621a5f19b0105591e3d
José Bollo <jose.bollo@iot.bzh>
2015-10-01 15:32:36 +0200

    improvement of qemu.sh
    Change-Id: I9375de37a8d944d4dd653f423985142aa0f9bf85

--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,10 @@
-qemu-system-x86_64 -cpu qemu64,+ssse3 -m 512 -usb --append "root=/dev/hda security=smack console=ttyS0" -k fr -serial stdio -kernel tmp/deploy/images/qemux86-64/bzImage-qemux86-64.bin -hda tmp/deploy/images/qemux86-64/agl-demo-platform-qemux86-64.ext3
+#!/bin/bash
+
+if [[ -d build ]]; then cd build; fi
+cd tmp/deploy/images/qemux86-64
+qemu-system-x86_64 \
+  -cpu qemu64,+ssse3 -m 512 -usb \
+  -k fr -serial stdio \
+  --append "root=/dev/hda security=smack console=ttyS0" \
+  -kernel bzImage-qemux86-64.bin \
+  -hda agl-demo-platform-qemux86-64.ext3
