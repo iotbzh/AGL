190d93430947baa4a998cd6690f0336eaaef9390
José Bollo <jose.bollo@iot.bzh>
2015-10-06 10:04:57 +0200

    Refactoring the function copy_mum_zip
    This rewrite allows now to switch to a board
    without having proprietaries files.
    
    Change-Id: I6fe527a6113420a0b69747f33f974d12e5a582cf

--- env.sh
+++ env.sh
@@ -1,34 +1,45 @@
 # This script intended to be sourced by another one.
 
 # syntax:
+#  unzip_proprietaries
+function unzip_proprietaries
+{
+	[ -d binary-tmp ] && return 0
+	echo -e "\n"
+	echo -e "Please download these 2 files from \"http://www.renesas.com/secret/r_car_download/rcar_demoboard.jsp\" and copy them into your \"$HOME\" directory:"
+	echo -e "* R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip"
+	echo -e "* R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip"
+	echo -e "and press [ENTER] to continue...\n"
+	read
+	[ -f "$HOME/R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip" ] && \
+	[ -f "$HOME/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip" ] && \
+	mkdir binary-tmp && \
+	unzip -o "$HOME/R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip" -d binary-tmp && \
+		unzip -o "$HOME/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip" -d binary-tmp
+}
+
+# syntax:
 #  copy_mum_zip <board-name> (ex: porter, silk)
 function copy_mum_zip
 {
 	board=$1
 
-	[ ! -d binary-tmp ] &&
-	{
-		echo -e "\n"
-		echo -e "Please download these 2 files from \"http://www.renesas.com/secret/r_car_download/rcar_demoboard.jsp\" and copy them into your \"$HOME\" directory:"
-		echo -e "* R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip"
-		echo -e "* R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip"
-		echo -e "and press [ENTER] to continue...\n"
-		read
-		mkdir binary-tmp
-		cd binary-tmp
-		unzip -o "$HOME/R-Car_Series_Evaluation_Software_Package_for_Linux-20150727.zip"
-		unzip -o "$HOME/R-Car_Series_Evaluation_Software_Package_of_Linux_Drivers-20150727.zip"
-		cd -
-	}
-
-	if [ -f meta-renesas/meta-rcar-gen2/copy_gfx_software_$board.sh ]; then
-		cd meta-renesas/meta-rcar-gen2
-		./copy_gfx_software_$board.sh ../../binary-tmp
-		./copy_mm_software_lcb.sh ../../binary-tmp
-		cd -
-	else
-		echo "Board name \"$board\" not found"
-		exit 1
+	if [ ! -f meta-renesas/meta-rcar-gen2/copy_gfx_software_$board.sh ]; then
+		echo "Multimedia installer for board name \"$board\" not found"
+		return 1
 	fi
+
+	if ! unzip_proprietaries; then
+		echo "Multimedia proprietaries files not found"
+		return 1
+	fi
+
+	cd meta-renesas/meta-rcar-gen2
+	if [ ! -f .last-board-set ] || [ $(cat .last-board-set) != $board ]; then
+		./copy_gfx_software_$board.sh ../../binary-tmp && \
+		./copy_mm_software_lcb.sh ../../binary-tmp && \
+		echo -n $board > .last-board-set
+	fi
+	cd -
 }
 
