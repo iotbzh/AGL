ad1d0edcdb6fb91ea45d475eddd00bd33947694c
José Bollo <jose.bollo@iot.bzh>
2015-10-22 10:04:14 +0200

    allow to name the image
    Change-Id: I69dfcffd4dbd90858e7608d922ff5025bcb360f4

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+image=${1:-agl-demo-platform}
+
+
+echo $image
+
 if [[ -d build ]]; then cd build; fi
 cd tmp/deploy/images/qemux86-64
 qemu-system-x86_64 \
@@ -7,4 +12,4 @@ qemu-system-x86_64 \
   -k fr -serial stdio \
   --append "root=/dev/hda security=smack console=ttyS0" \
   -kernel bzImage-qemux86-64.bin \
-  -hda agl-demo-platform-qemux86-64.ext3
+  -hda ${image}-qemux86-64.ext3
