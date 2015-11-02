c427b31745b6fbf4278614c0a9b35021b04a40de
Yannick Gicquel <yannick.gicquel@iot.bzh>
2015-11-02 16:22:02 +0100

    fetchall.sh helper script
    Signed-off-by: Yannick Gicquel <yannick.gicquel@iot.bzh>

---
+++ fetchall.sh
@@ -0,0 +1,45 @@
+#/bin/bash
+
+LANG=
+list=$(echo $PWD/*/.git)
+
+#----------------------------------------
+# check that repos are on clean commits
+clean=true
+for x in $list
+do
+  if [[ -d $x ]]
+  then
+    cd $x/..
+    n=$(basename $PWD)
+    if git status --porcelain -b | grep -qv '^##'
+    then
+      clean=false
+      echo "error: submodule $n has local modication(s):" >&2
+      git status -s | sed "s#^.. #   &$n/#" >&2
+    fi
+  fi
+done
+$clean || exit 1
+
+#----------------------------------------
+# emit the repo state
+for x in $list
+do
+  if [[ -d $x ]]
+  then
+    cd $x/..
+    #n=$(basename $PWD)
+    #b=$(git status --porcelain -b | sed 's:\.\.\..*::' | awk '{print $2}')
+    #c=$(git log --pretty=format:%h -1)
+    r=$(git remote -v | awk '{print $1}' | sort -u)
+    for g in $r
+    do
+      echo ----- $PWD
+      echo git fetch -p -t $g
+      git fetch -p -t $g
+    done
+  fi
+done
+
+
