fe9ae6de8c17d5d871482c0baf908b8a2006010a
José Bollo <jose.bollo@iot.bzh>
2016-02-05 13:56:07 +0100

    use better start script
    Change-Id: I9169355dc3db6947a635f1f98aa91ab6a778971f
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- qemu.sh
+++ qemu.sh
@@ -6,10 +6,10 @@ image=${1:-agl-demo-platform}
 echo $image
 
 if [[ -d build ]]; then cd build; fi
-cd tmp/deploy/images/qemux86-64
-qemu-system-x86_64 \
-  -cpu qemu64,+ssse3 -m 512 -usb \
-  -k fr -serial stdio \
-  --append "root=/dev/hda security=smack console=ttyS0" \
-  -kernel bzImage-qemux86-64.bin \
-  -hda ${image}-qemux86-64.ext4
+
+exec runqemu \
+	qemux86-64 \
+	tmp/deploy/images/qemux86-64/bzImage-qemux86-64.bin \
+	tmp/deploy/images/qemux86-64/agl-demo-platform-qemux86-64.ext4 \
+	serial
+
