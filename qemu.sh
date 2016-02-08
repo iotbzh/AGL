5e59de0874ad228ba7ba61d19cd0db229bddd0b2
José Bollo <jose.bollo@iot.bzh>
2016-02-08 22:10:43 +0100

    qemu.sh: minor improvement
    This improvement requires to also patch
    runqemu-internal
    
    Change-Id: I7ad240e5f70dbfc3dfc7bd4d6bd96bc03c3fbec8
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- qemu.sh
+++ qemu.sh
@@ -7,9 +7,12 @@ echo $image
 
 if [[ -d build ]]; then cd build; fi
 
+export QEMU_UI_OPTIONS="-usb -usbdevice mouse"
 exec runqemu \
+	qemuparams="-m 512" \
 	qemux86-64 \
 	tmp/deploy/images/qemux86-64/bzImage-qemux86-64.bin \
 	tmp/deploy/images/qemux86-64/agl-demo-platform-qemux86-64.ext4 \
-	serial
+	serial \
+	bootparams="uvesafb.mode_option=1024x768-32"
 
