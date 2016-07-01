f65c1c954ed69ff85029805c74598ea3634ad899
José Bollo <jose.bollo@iot.bzh>
2016-07-01 14:32:23 +0200

    more cpus
    Change-Id: I4ec3ef16c80f62ce1ead1c04813b54388273ea9f
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- qemu.sh
+++ qemu.sh
@@ -9,7 +9,7 @@ if [[ -d build ]]; then cd build; fi
 
 export QEMU_UI_OPTIONS="-usb -usbdevice mouse"
 exec runqemu \
-	qemuparams="-m 512" \
+	qemuparams="-m 512 -cpu Nehalem -smp 4" \
 	qemux86-64 \
 	tmp/deploy/images/qemux86-64/bzImage-qemux86-64.bin \
 	tmp/deploy/images/qemux86-64/agl-demo-platform-qemux86-64.ext4 \
